e8e3ce6b32f4c87c45f8a524e3f4ec6f46b312aa
cats 4 life3 <98923511+reyyymart@users.noreply.github.com>
2023-12-02 19:06:04 +0800

    Add files via upload

---
+++ RockAndPaper.py
@@ -0,0 +1,61 @@
+#python 3.7.1
+import random
+import json
+print("Rock and paper game by Reymart")
+
+score = {
+  "Computer": 0,
+  "You": 0,
+  "Tie": 0
+}
+choices = {
+  "1": "Rock",
+  "2": "Paper",
+  "3": "Scissor"
+}
+youDecide = {
+  "Rock": "Scissor",
+  "Paper": "Rock",
+  "Scissor": "Paper"
+}
+
+def DecisionMaker(you, computer):
+  print("-------------------------------------")
+  winner = ""
+  if youDecide[you]:
+    bot = youDecide[you]
+    print(f'you: {you} / computer: {computer}')
+    if you == computer:
+      print("it's a tie!")
+      winner = "Tie"
+    else:
+      if computer == bot:
+        print("you win :)")
+        winner = "You"
+      else:
+        print("you lose :(")
+        winner = "Computer"
+  score[winner] += 1
+  print(f'YOU = {score["You"]} / COMPUTER = {score["Computer"]} / TIE = {score["Tie"]}')
+  print("-------------------------------------")
+
+def StartInput():
+  print("Choose one")
+  bot = random.randint(1,len(choices))
+  for i in choices:
+    print(i,"=", choices[i])
+  answer = input("Your input:")
+  if answer.isnumeric() == bool(1) and int(answer) < len(choices)+1:
+    if choices[answer]:
+      you = choices[answer]
+      computer = choices[str(bot)]
+      DecisionMaker(you, computer)
+  else:
+    error = {
+      "code" : 101,
+      "reason" : answer+" is not in the choices"
+    }
+    print("error",error)
+  return StartInput()
+StartInput()
+
